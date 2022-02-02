7008ff2b6292f61cafdd9f938ed875d118b374fd
heimusharper <heimusharper@gmail.com>
2022-02-02 18:20:29 +0400

    cxx 17

---
+++ build/CMakeFiles/VEGAVideoServer.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/unmanned/git/VEGAVideoServer/FFH264DecoderInstance.cpp" "/home/unmanned/git/VEGAVideoServer/build/CMakeFiles/VEGAVideoServer.dir/FFH264DecoderInstance.cpp.o"
+  "/home/unmanned/git/VEGAVideoServer/FFImageFrame.cpp" "/home/unmanned/git/VEGAVideoServer/build/CMakeFiles/VEGAVideoServer.dir/FFImageFrame.cpp.o"
+  "/home/unmanned/git/VEGAVideoServer/FFImageHttpSink.cpp" "/home/unmanned/git/VEGAVideoServer/build/CMakeFiles/VEGAVideoServer.dir/FFImageHttpSink.cpp.o"
+  "/home/unmanned/git/VEGAVideoServer/FFPlayerInstance.cpp" "/home/unmanned/git/VEGAVideoServer/build/CMakeFiles/VEGAVideoServer.dir/FFPlayerInstance.cpp.o"
+  "/home/unmanned/git/VEGAVideoServer/HTTPServer.cpp" "/home/unmanned/git/VEGAVideoServer/build/CMakeFiles/VEGAVideoServer.dir/HTTPServer.cpp.o"
+  "/home/unmanned/git/VEGAVideoServer/MavContext.cpp" "/home/unmanned/git/VEGAVideoServer/build/CMakeFiles/VEGAVideoServer.dir/MavContext.cpp.o"
+  "/home/unmanned/git/VEGAVideoServer/MavUDP.cpp" "/home/unmanned/git/VEGAVideoServer/build/CMakeFiles/VEGAVideoServer.dir/MavUDP.cpp.o"
+  "/home/unmanned/git/VEGAVideoServer/MavlinkParser.cpp" "/home/unmanned/git/VEGAVideoServer/build/CMakeFiles/VEGAVideoServer.dir/MavlinkParser.cpp.o"
+  "/home/unmanned/git/VEGAVideoServer/main.cpp" "/home/unmanned/git/VEGAVideoServer/build/CMakeFiles/VEGAVideoServer.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "."
+  "../"
+  "../c_library_v2"
+  "/usr/include/gdal"
+  "/usr/local/include"
+  "/usr/include/aarch64-linux-gnu"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
